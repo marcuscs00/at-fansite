aadf3a4370007efb5b05bb704e33fab0e55e2b0b
Marcus Smolenski <marcussmolenski00@gmail.com>
2022-02-27 09:52:58 -0500

    Added a gitignore

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ icbyd = 'https://www.youtube.com/watch?v=NJesxnyCMbU';
 rdja = 'https://www.youtube.com/watch?v=bIoI0yE0aIo&list=PLiN-7mukU_RHfppe7hjRD6WU06C9ib-T4';
 drukqs = 'https://www.youtube.com/watch?v=htUalPYmIxk&list=PL5SMXYhIcZ5IeYo5XP4haf5aJmj9TWs8b';
 syro = 'https://www.youtube.com/watch?v=yl_zy389rbk&list=PL5SMXYhIcZ5KrbqW82yRAFgzoYcblFCFt';
+
+yt_discog_list = [saw1,saw2,icbyd,rdja,drukqs,syro];

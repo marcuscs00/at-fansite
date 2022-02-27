f4e415c3bd586454da98d232a4cef6d3bb30b257
Marcus Smolenski <marcussmolenski00@gmail.com>
2022-02-27 09:51:55 -0500

    Added links for album and made 1 array, updated discography

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// hi
+saw1 = 'https://www.youtube.com/watch?v=Xw5AiRVqfqk';
+saw2 = 'https://www.youtube.com/watch?v=-57Ti2i_yhU';
+icbyd = 'https://www.youtube.com/watch?v=NJesxnyCMbU';
+rdja = 'https://www.youtube.com/watch?v=bIoI0yE0aIo&list=PLiN-7mukU_RHfppe7hjRD6WU06C9ib-T4';
+drukqs = 'https://www.youtube.com/watch?v=htUalPYmIxk&list=PL5SMXYhIcZ5IeYo5XP4haf5aJmj9TWs8b';
+syro = 'https://www.youtube.com/watch?v=yl_zy389rbk&list=PL5SMXYhIcZ5KrbqW82yRAFgzoYcblFCFt';

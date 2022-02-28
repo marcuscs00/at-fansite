7e189b4ba4404f6ff6082c844d4ad37a5bd60721
Marcus Smolenski <marcussmolenski00@gmail.com>
2022-02-28 10:59:02 -0500

    Added left/right functionality

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
-saw1 = 'https://www.youtube.com/watch?v=Xw5AiRVqfqk';
-saw2 = 'https://www.youtube.com/watch?v=-57Ti2i_yhU';
-icbyd = 'https://www.youtube.com/watch?v=NJesxnyCMbU';
-rdja = 'https://www.youtube.com/watch?v=bIoI0yE0aIo&list=PLiN-7mukU_RHfppe7hjRD6WU06C9ib-T4';
-drukqs = 'https://www.youtube.com/watch?v=htUalPYmIxk&list=PL5SMXYhIcZ5IeYo5XP4haf5aJmj9TWs8b';
-syro = 'https://www.youtube.com/watch?v=yl_zy389rbk&list=PL5SMXYhIcZ5KrbqW82yRAFgzoYcblFCFt';
+saw1 = 'https://www.youtube.com/embed/Xw5AiRVqfqk';
+saw2 = 'https://www.youtube.com/embed/-57Ti2i_yhU';
+icbyd = 'https://www.youtube.com/embed/NJesxnyCMbU';
+rdja = 'https://www.youtube.com/embed/bIoI0yE0aIo&list=PLiN-7mukU_RHfppe7hjRD6WU06C9ib-T4';
+drukqs = 'https://www.youtube.com/embed/htUalPYmIxk&list=PL5SMXYhIcZ5IeYo5XP4haf5aJmj9TWs8b';
+syro = 'https://www.youtube.com/embed/yl_zy389rbk&list=PL5SMXYhIcZ5KrbqW82yRAFgzoYcblFCFt';
 
 yt_discog_list = [saw1,saw2,icbyd,rdja,drukqs,syro];
+num = 0
 
 $(function(){
-
+    
     // jQuery methods go here...
-    $("button").click(function(){
-        $("iframe").prop('src') = saw2;
+    $("#back").click(function(){
+        num -= 1;
+        if (num < 0) {
+            num = 5;
+        }
+        $("iframe").attr("src", yt_discog_list[num]);
+      });
+    $("#next").click(function(){
+        num += 1;
+        if (num > 5) {
+            num = 0;
+        }
+        $("iframe").attr("src", yt_discog_list[num]);
       });
   });

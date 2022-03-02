77cf1995829f7cb25cdecaebf3344dbdcad2e594
Marcus Smolenski <marcussmolenski00@gmail.com>
2022-03-02 07:49:50 -0500

    Added embeded playlists

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 saw1 = 'https://www.youtube.com/embed/Xw5AiRVqfqk';
 saw2 = 'https://www.youtube.com/embed/-57Ti2i_yhU';
 icbyd = 'https://www.youtube.com/embed/NJesxnyCMbU';
-rdja = 'https://www.youtube.com/embed/bIoI0yE0aIo&list=PLiN-7mukU_RHfppe7hjRD6WU06C9ib-T4';
-drukqs = 'https://www.youtube.com/embed/htUalPYmIxk&list=PL5SMXYhIcZ5IeYo5XP4haf5aJmj9TWs8b';
-syro = 'https://www.youtube.com/embed/yl_zy389rbk&list=PL5SMXYhIcZ5KrbqW82yRAFgzoYcblFCFt';
+rdja = 'https://www.youtube.com/embed/videoseries?list=PLiN-7mukU_RHfppe7hjRD6WU06C9ib-T4';
+drukqs = 'https://www.youtube.com/embed/videoseries?list=PL5SMXYhIcZ5IeYo5XP4haf5aJmj9TWs8b';
+syro = 'https://www.youtube.com/embed/videoseries?list=PL5SMXYhIcZ5KrbqW82yRAFgzoYcblFCFt';
 
 yt_discog_list = [saw1,saw2,icbyd,rdja,drukqs,syro];
 num = 0
